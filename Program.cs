29c27492d7c78f44861b0b6276d16bdd313b09ab
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2024-09-04 01:00:31 +0200

    Service with LocalStorage functionality and use of Subjects

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazorWebAssemblyApp;
@@ -8,4 +11,27 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<SharedStateService>();
+builder.Services.AddBlazoredLocalStorage();
+
 await builder.Build().RunAsync();
+public class SharedStateService
+{
+    private readonly ILocalStorageService localStorage;
+    public SharedStateService(ILocalStorageService LocalStorage)
+    {
+        localStorage = LocalStorage;
+    }
+
+    public readonly BehaviorSubject<string> value = new("Test");
+
+
+    public async Task OnInitializedAsync()
+    {
+        var someSharedDataMaybe = await localStorage.GetItemAsStringAsync("message");
+        if (someSharedDataMaybe is not null)
+        {
+            value.OnNext(someSharedDataMaybe);
+        }
+    }
+}
